307ec719956a1d053d5728a76148ed05b65a2c30
2541773-wits <133588025+2541773-wits@users.noreply.github.com>
2024-04-14 14:48:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function registration_validation(){
+    let emailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(document.form1.Username.value.length < 3){
+        document.getElementById("result").style.visibility = "visible";
+        document.getElementById("result").innerHTML = "Username should be at least 3 characters"
+        return false;
+    }
+    else if(!emailFormat.test(document.form1.Email.value)){
+        document.getElementById("result").style.visibility = "visible";
+        document.getElementById("result").innerHTML = "Email is invalid!"
+        return false;
+    }
+    else if(document.form1.Password.value.length < 8){
+        document.getElementById("result").style.visibility = "visible";
+        document.getElementById("result").innerHTML = "Password needs to be at least 8 characters";
+        return false;
+    }
+    else if(document.form1.Password.value !== document.form1.cPassword.value){
+        document.getElementById("result").style.visibility = "visible";
+        document.getElementById("result").innerHTML = "Password does not match";
+        return false;
+    }
+    else if(document.form1.Password.value === document.form1.cPassword.value){
+        let popup = document.getElementById("popup");
+        popup.classList.add("open-slide");
+        return false;
+    }
+}
+
+function closeSlide(){
+    popup.classList.remove('open-slide');
+    if(document.getElementById("result").style.visibility === "visible"){
+        document.getElementById("result").innerHTML = "";
+        document.getElementById("result").style.visibility = "hidden";
+    }
+}
+
