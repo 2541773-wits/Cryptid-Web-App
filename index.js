62cd25dba03af534eb88eb37174afcc1721a225e
Ruaan1 <2446659@students.wits.ac.za>
2024-04-17 17:02:19 +0200

    G_Button on both, and names changed

--- index.js
+++ index.js
@@ -16,20 +16,19 @@ import { getAuth, GoogleAuthProvider,signInWithPopup } from "https://www.gstatic
  auth.languageCode = 'en';
  const provider = new GoogleAuthProvider();
 
- const googleLogin = document.getElementById("google-login-btn");
+ const googleSignin = document.getElementById("google-signin-btn");
 
- googleLogin.addEventListener("click",function(){
-   signInWithPopup(auth, provider)
- .then((result) => {
-   const user = result.user;
-   console.log(user);
-   window.location.href ="../logged.html";
- }).catch((error) => {
-   const errorCode = error.code;
-   const errorMessage = error.message;
-   
- });
- })
+ googleSignin.addEventListener("click",function(){
+  signInWithPopup(auth, provider)
+.then((result) => {
+  const user = result.user;
+  console.log(user);
+  window.location.href ="../Loggedin.html";
+}).catch((error) => {
+  const errorCode = error.code;
+  const errorMessage = error.message;
+  
+});
+})
 
  
-
