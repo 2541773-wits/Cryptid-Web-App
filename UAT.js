61f8fac990244bcf9f913cdf7471b6ba83a4a98b
2541773-wits <2541773@students.wits.ac.za>
2024-04-22 11:38:24 +0000

    Create user acceptance tests

---
+++ UAT.js
@@ -0,0 +1,15 @@
+function existingUsername_registration_prevent(){
+
+}
+
+function nonexistentUsername_login_prevent(){
+
+}
+
+function correctDetails_login_allow(){
+
+}
+
+function incorrectPass_login_prevent(){
+    
+}
