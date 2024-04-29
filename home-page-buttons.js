4a81bde544d3c68bb5906da517bb1b9347524756
2541773-wits <2541773@students.wits.ac.za>
2024-04-29 09:56:32 +0200

    Add link to game page

--- home-page-buttons.js
+++ home-page-buttons.js
@@ -64,4 +64,16 @@ document.addEventListener("DOMContentLoaded", function(){
     });
       
 
+
+    const startBtn = document.getElementById('start');
+    startBtn.addEventListener("click",()=>{
+       // window.location.href = "game.html";
+        let selectElement = document.getElementById("num-plyers");
+        let numPlayers = selectElement.options[selectElement.selectedIndex].value;
+        let switchElement = document.getElementById("switch");
+        let advanced = switchElement.checked;
+        sessionStorage.setItem("advanced",advanced);
+        sessionStorage.setItem("numPlayers",numPlayers);
+        window.location.href = "game.html";
+    })
 });
